33b823c2c3c2cd96c6d9697bd81684c45f998e4b
yunli.jessica <yunli.jessica@gmail.com>
2016-09-24 20:37:29 -0700

    Delete something here

--- Podfile
+++
@@ -1,9 +0,0 @@
-platform :ios, '9.0'
-use_frameworks!
-
-target 'FaithMingle' do
-pod 'AFNetworking'
-pod 'Calendar-iOS'
-pod 'CVCalendar'
-pod 'UIColor+Hex'
-end
--- Pods/Target Support Files/Calendar-iOS/Calendar-iOS-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "CalendarView.h"
-#import "NSDate+CalendarView.h"
-#import "NSString+CalendarView.h"
-
-FOUNDATION_EXPORT double Calendar_iOSVersionNumber;
-FOUNDATION_EXPORT const unsigned char Calendar_iOSVersionString[];
-
